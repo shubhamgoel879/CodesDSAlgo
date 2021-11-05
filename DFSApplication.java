3f3bde81aefd544dc6010f3ea0b69b3d45d6ac6e
shubhamgoel879 <90336785+shubhamgoel879@users.noreply.github.com>
2021-11-05 23:41:19 +0530

    Update DFSApplication.java

--- DFSApplication.java
+++ DFSApplication.java
@@ -1,5 +1,5 @@
 /*Given a NxM matrix containing Uppercase English Alphabets only. Your task is to tell if there is a path in the given matrix which makes the sentence “CODINGNINJA” .
-There is a path from any cell to all its neighbouring cells.*/
+You can move from a cell to its 8 neighbouring cells*/
 
 
 public class Solution {
